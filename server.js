0868263616a6c2ea67d046d5306d43f88e989c36
OpkWan <alexopoku83@gmail.com>
2024-07-18 19:47:28 +0000

    Working Rappers Api

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const app = express()
+
+const PORT = 8000
+
+const rapper = {
+    "21 savage": {
+        "age": 29,
+    "birthName": "Sheyaa Bin Abraham-Joseph",
+    "birthLocation": "London, England"
+    },
+    "chance the rapper": {
+        "age": 29,
+    "birthName": "Chancelor Bennett",
+    "birthLocation": "Chicago, Illinois"
+    },
+    "unknown": {
+        "age": 0,
+    "birthName": "unknown",
+    "birthLocation": "unknown"
+    }
+}
+
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.get("/api/:rapperName", (req,res)=>{
+    const rappersName = req.params.rapperName.toLocaleLowerCase()
+    if(rapper[rappersName]){
+        res.json(rapper[rappersName])
+    }else{
+        res.json(rapper["unknown"])
+    }
+    // console.log(rapper[rappersName]);
+    
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
